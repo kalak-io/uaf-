610f61c24ff2da18b3f4ce1a18de75823fd132cf
cjacquem <cjacquem@student.42.fr>
2017-08-25 10:47:33 +0200

    Create dict of archivers and their command

--- archives_extractor.py
+++ archives_extractor.py
@@ -3,24 +3,49 @@
 
 import argparse
 
-EXT_ARCHIVERS = ['.zip', '.rar', '.bz2', '.gz', '.tar', '.tbz2', '.tgz', '.Z',
-                 '.7z', '.xz', '.exe', '.tar.bz2', '.tar.gz', '.tar.xz',
-                 '.arj', '.cab', '.chm', '.deb', '.dmg', '.iso', '.lzh',
-                 '.msi', '.rpm', '.udf', '.wim', '.xar'] 
+ARCHIVERS = {
+             '.arj': 'arj e',
+             '.bz2': 'bzip2 -d',
+             '.cab': 'cabextract -d',
+             '.chm': 'archmage',
+             '.deb': 'ar x',
+             '.dmg': '7z x',
+             '.exe': '7z e',
+             '.gz': 'gzip -d',
+             '.iso': '7z x',
+             '.lzh': '7z x',
+             '.msi': '7z x',
+             '.rar': 'unrar x',
+             '.rpm': 'rpm2cpio',
+             '.tar': 'tar -xvf',
+             '.tar.bz2': 'tar -jxvf',
+             '.tar.gz': 'tar -zxvf',
+             '.tar.xz': 'tar xf',
+             '.tbz': 'tar -jxvf',
+             '.tbz2': 'tar -jxvf',
+             '.tgz': 'tar -xvzf',
+             '.udf': '',
+             '.wim': '',
+             '.xar': 'xar -x',
+             '.xz': 'tar xf',
+             '.zip': 'unzip',
+             '.Z': 'uncompress',
+             '.7z': '7za x',
+            }
 
-def grooming_list_files(lst_files):
-    return lst_files
-    
 
-def archives_extractor(lst_files):
-    print(lst_files)
+def archives_extractor(f):
+    print(f)
+
+
+def main():
+    list(map(archives_extractor, args.files))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', nargs='+',
+    parser.add_argument('-f', '--files', nargs='+',
                         help='Type path of the different files to extract',
                         required=True)
     args = parser.parse_args()
-    args.file = grooming_list_files(args.file)
-    archives_extractor(args.file)
+    main()
