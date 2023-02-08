202d2dd91ba32d3f89fe203236964777e8168744
Clément Jacquemaire <clement@unifai.fr>
2023-02-08 23:53:42 +0100

    :art: ADD zip extension

--- constants.py
+++ constants.py
@@ -13,37 +13,52 @@ TAR = {
     }
 
 TAR_GZ = {
-        "extract": {
-            "package": "tar",
-            "options": "xvzf",
-            "pattern": "{package} {options} {sources}"
-        },
         "compact": {
             "package": "tar",
             "options": "cvzf",
             "pattern": "{package} {options} {destination} {sources}"
         },
+        "extract": {
+            "package": "tar",
+            "options": "xvzf",
+            "pattern": "{package} {options} {sources}"
+        },
         "suffix": [".tar.gz", ".tgz"]
     }
 
 TAR_BZ = {
-        "extract": {
-            "package": "tar",
-            "options": "xvjf",
-            "pattern": "{package} {options} {sources}"
-        },
         "compact": {
             "package": "tar",
             "options": "cvjf",
             "pattern": "{package} {options} {destination} {sources}"
         },
+        "extract": {
+            "package": "tar",
+            "options": "xvjf",
+            "pattern": "{package} {options} {sources}"
+        },
         "suffix": [".tar.tbz2", ".tar.bz2"]
     }
+
+ZIP = {
+    "compact": {
+        "package": "zip",
+        "options": "-d",
+        "pattern": "{package} {options} {destination} {sources}"
+    },
+    "extract": {
+        "package": "unzip",
+        "options": "-d",
+        "pattern": "{package} {options} {destination} {sources}"
+    },
+    "suffix": [".zip"]
+}
 
 CONFIG_MIME_TYPE = {
     ('application/x-tar', None): TAR,
     ('application/x-tar', 'xz'): TAR,
     ('application/x-tar', 'gzip'): TAR_GZ,
-    ('application/x-tar', 'bzip2'): TAR_BZ
+    ('application/x-tar', 'bzip2'): TAR_BZ,
+    ('application/zip', None): ZIP
 }
  
