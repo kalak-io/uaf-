6683e1955e3283171ba7a7137de3b8e5bfd5c3b6
Clément Jacquemaire <clement@unifai.fr>
2023-02-08 23:54:48 +0100

    :white_check_mark: ADD tests about utils

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+import utils
+
+class TestValidateFilepath:
+    def test_wrong_filepath(self):
+        result = utils.validate_filepath("/bab/path/archive.tar")
+        assert result == False
+
+    def test_right_filepath(self, tmp_path):
+        result = utils.validate_filepath(f'{tmp_path}/archive.tar')
+        assert result == True
