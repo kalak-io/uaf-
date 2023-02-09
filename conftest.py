497cd5f47942874b37c3eee49df227cccc728f72
Clément Jacquemaire <clement@unifai.fr>
2023-02-09 17:01:23 +0100

    :art: ADD pragma python files

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import pytest
 from pathlib import Path
 
--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 TAR = {
         "compact": {
             "package": "tar",
